b67a5cbb9174c27f761dea9a68ac93cbebded048
antonioCB14 <antonioromon@hotmail.com>
2018-10-25 14:27:54 +0200

    Se añade descripción

--- Tema 3/WS3/puzzleindex.js
+++ Tema 3/WS3/puzzleindex.js
@@ -1,3 +1,16 @@
+/*1) Puzzle. Se desea implementar una web para la realización de puzzles. Un puzzle no es
+más que un tablero cuadrado con un hueco que podemos mover y que permite
+reordenar las piezas. Se pide por tanto la implementación de una clase que represente
+este juego teniendo en cuenta:
+1. La dimensión puede variar, se escogerá en la creación.
+2. El espacio en blanco sólo se mueve arriba, abajo, izquierda, derecha, controlando
+por supuesto que sea un movimiento válido.
+3. Debe llevarse un control del tiempo mínimo para resolverlo, así como el número de
+movimientos realizados.
+4. Los tableros se generarán aleatoriamente.
+5. Implementarás un método dibujar() que imprimirá en pantalla el tablero para poder
+ser probado. */
+
 var nfilas = prompt("Introduce el numero de filas:");
 var ncolumnas = prompt("Introduce el numero de columnas:");
 var tablero = [];
