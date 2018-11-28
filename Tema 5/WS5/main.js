ea7e4b79461e101fb1482a3bc44aaa9212dc4b05
antonioCB14 <antonioromon@hotmail.com>
2018-11-28 11:04:44 +0100

    Sticky notes

--- Tema 5/WS5/main.js
+++ Tema 5/WS5/main.js
@@ -1,9 +1,22 @@
-import {Nota} from './nota.js';
+class Nota{
+    constructor(titulo, texto){
+        this.titulo = titulo;
+        this.texto = texto;
+        this.hora = new Date().getHours() +":"+new Date().getMinutes(); 
+        this.timestamp = new Date().getTime()
+    }
+}
 
 var arrayNotas = [];
 var panel = document.getElementById("panel");
+if (localStorage.getItem("array") != null){
+    arrayNotas = JSON.parse(localStorage.getItem("array"));
+    todasNotas(arrayNotas);
+}
+
 
 function todasNotas(notas){
+    localStorage.setItem("array", JSON.stringify(arrayNotas));
     while (panel.firstChild) {
         panel.removeChild(panel.firstChild);
     }
@@ -17,13 +30,37 @@ function vistaNota(nota){
     div.setAttribute('class',"nota");
     div.innerHTML = "<h2>"+nota.titulo+"</h2>"+
                     "<p>"+nota.texto+"</p>"+
-                    "<h6>"+nota.hora+"</h6>";
+                    "<h6>"+nota.hora+"</h6>"+
+                    "<input type='button' id='eliminar' value='Eliminar' onclick='borrarNota("+nota.timestamp+")'>"+
+                    "<input type='button' id='editar' value='Editar' onclick='editarNota("+nota.timestamp+")'>";
     return div;
 }
 
-document.getElementById("boton").addEventListener("click", crearNota);
 function crearNota(){
     var nuevaNota = new Nota(document.getElementById("titulo").value, document.getElementById("texto").value);
     arrayNotas.push(nuevaNota);
+    localStorage.setItem("array", JSON.stringify(arrayNotas));
+    panel.appendChild(vistaNota(arrayNotas[arrayNotas.length-1]));
+}
+
+function borrarNota(date){
+    for(var i=0;i<arrayNotas.length;i++){
+        if(arrayNotas[i].timestamp == date){
+            arrayNotas.splice(i,1);
+            break;
+        }
+    }
+    todasNotas(arrayNotas);
+}
+
+function editarNota(date){
+    for(var i=0;i<arrayNotas.length;i++){
+        if(arrayNotas[i].timestamp == date){
+            var tituloEdit = prompt("Introduce el nuevo titulo");
+            var textoEdit = prompt("Introduce el nuevo texto");
+            arrayNotas[i] = new Nota(tituloEdit,textoEdit);
+            break;
+        }
+    }
     todasNotas(arrayNotas);
 }
--- Tema 5/WS5/nota.js
+++
@@ -1,7 +0,0 @@
-export class Nota{
-    constructor(titulo, texto){
-        this.titulo = titulo;
-        this.texto = texto;
-        this.hora = new Date().getHours()+":"+new Date().getMinutes(); 
-    }
-}
